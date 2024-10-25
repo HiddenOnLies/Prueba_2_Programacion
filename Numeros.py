cb4524b2ba4b247a32a78606da6b4b571ea35e94
HiddenOnLies <hiddenonlies@gmail.com>
2024-10-24 22:29:39 -0300

    V1.0

--- Numeros.py
+++ Numeros.py
@@ -1,26 +1,44 @@
 #Autores: Alejandro Inzulza y Eduardo Olguin
 def abrir_archivo(nombre_archivo):
-    archivo = open(nombre_archivo, "r")                           
-    contenido = archivo.read().split("\n")
+    salida = []
+    archivo = open(nombre_archivo, "r")   
+    contenido = archivo.readlines()
+    for lineas in contenido:
+        salida.append(lineas.strip().split())
     archivo.close()
-    return contenido
-         
+    return salida
+        
 def identificar_numeros(palabras, numeros_palabras, escalas_palabras):
     numeros_digitados = []
-    total = 0
-    suma_temporal = 0
-            
+    
+    for numeros in palabras:
+        suma_temporal = 0
+        es_negativo = False
 
-def generar_salida():
-    pass
-                
-            
+        for num in numeros:
+            if num in numeros_palabras:
+                suma_temporal += numeros_palabras[num]
+            elif num in escalas_palabras:
+                suma_temporal *= escalas_palabras[num]
+            elif num == "negative":
+                es_negativo = True
+        
+        if es_negativo == True:
+            suma_temporal *= -1
+        
+        numeros_digitados.append(suma_temporal)
+    
+    return numeros_digitados           
 
-
-
+def generar_salida(nombre_salida, numeros):
+    archivo = open(nombre_salida, "w")
+    for num in numeros:
+        archivo.write(str(num) + "\n")
+    archivo.close()                            
 
 if __name__=="__main__":
     nombre_archivo = "en_palabras.txt"
+    nombre_salida = "en_numeros.txt"
     numeros_palabras = {"zero": 0, "one":1, "two":2, "three": 3, "four": 4, "five": 5,
                         "six": 6, "seven": 7, "eight": 8, "nine": 9, "ten": 10, "eleven": 11,
                         "twelve": 12, "thirteen": 13, "fourteen": 14, "fifteen": 15, "sixteen": 16,
@@ -29,4 +47,5 @@ if __name__=="__main__":
     escalas_palabras = {"hundred": 100, "thousand": 1000, "million": 1000000}
     palabras = abrir_archivo(nombre_archivo)
     numeros = identificar_numeros(palabras, numeros_palabras, escalas_palabras)
+    generar_salida(nombre_salida, numeros)
     
