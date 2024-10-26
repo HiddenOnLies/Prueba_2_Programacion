19be8c08c79914c4aeac9b2c7137ade793621410
HiddenOnLies <hiddenonlies@gmail.com>
2024-10-25 21:40:09 -0300

    V1.2
    Diccionarios acumulados en funciones, si el largo de la lista no es mayor o igual 1 no se agregara a la lista de salida de los numeros
    
    Recomendacion: cambiar nombre de variables y espacios para mejor comprendimiento del codigo

--- Numeros.py
+++ Numeros.py
@@ -4,18 +4,30 @@ def abrir_archivo(nombre_archivo):
     archivo = open(nombre_archivo, "r")   
     contenido = archivo.readlines()
     for lineas in contenido:
-        salida.append(lineas.strip().split())
+        palabras = lineas.strip(" ").split()
+        if len(palabras) >= 1:
+            salida.append(palabras)
     archivo.close()
+
     return salida
+
+def variables_numeros():
+    numeros = {"zero": 0, "one":1, "two":2, "three": 3, "four": 4, "five": 5,
+                "six": 6, "seven": 7, "eight": 8, "nine": 9, "ten": 10, "eleven": 11,
+                "twelve": 12, "thirteen": 13, "fourteen": 14, "fifteen": 15, "sixteen": 16,
+                "seventeen": 17, "eighteen": 18, "nineteen": 19,"twenty": 20, "thirty": 30,
+                "forty": 40, "fifty": 50, "sixty": 60, "seventy": 70, "eighty": 80, "ninety": 90}
+    
+    escalas = {"hundred": 100, "thousand": 1000, "million": 1000000}
+    
+    return numeros, escalas
         
 def identificar_numeros(palabras, numeros_palabras, escalas_palabras):
     numeros_digitados = []
-    
     for numeros in palabras:
         total = 0
         suma_temporal = 0
         es_negativo = False
-
         for num in numeros:
             if num in numeros_palabras:
                 suma_temporal += numeros_palabras[num]
@@ -27,14 +39,13 @@ def identificar_numeros(palabras, numeros_palabras, escalas_palabras):
                     suma_temporal = 0
             elif num == "negative":
                 es_negativo = True
-        
         total += suma_temporal
-
         if es_negativo == True:
             total *= -1
-        
+        else:
+            total *= 1
         numeros_digitados.append(total)
-    
+
     return numeros_digitados           
 
 def generar_salida(nombre_salida, numeros):
@@ -46,12 +57,7 @@ def generar_salida(nombre_salida, numeros):
 if __name__=="__main__":
     nombre_archivo = "en_palabras.txt"
     nombre_salida = "en_numeros.txt"
-    numeros_palabras = {"zero": 0, "one":1, "two":2, "three": 3, "four": 4, "five": 5,
-                        "six": 6, "seven": 7, "eight": 8, "nine": 9, "ten": 10, "eleven": 11,
-                        "twelve": 12, "thirteen": 13, "fourteen": 14, "fifteen": 15, "sixteen": 16,
-                        "seventeen": 17, "eighteen": 18, "nineteen": 19,"twenty": 20, "thirty": 30,
-                        "forty": 40, "fifty": 50, "sixty": 60, "seventy": 70, "eighty": 80, "ninety": 90 }
-    escalas_palabras = {"hundred": 100, "thousand": 1000, "million": 1000000}
+    numeros_palabras, escalas_palabras = variables_numeros()
     palabras = abrir_archivo(nombre_archivo)
     numeros = identificar_numeros(palabras, numeros_palabras, escalas_palabras)
     generar_salida(nombre_salida, numeros)
